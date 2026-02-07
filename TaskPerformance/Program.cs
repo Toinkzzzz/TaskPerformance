b41b4ed50fa30f4550797dbecab3a4c1ca83230a
clemente.222612 <clemente.222612@ANNEX-PC40>
2026-02-07 09:51:07 +0800

    Add project files.

---
+++ TaskPerformance/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ComputeAverageApp
+{
+    class ComputeAverageProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 5 grades separated by new line: ");
+
+            double englishGrade = Convert.ToDouble(Console.ReadLine());
+            double mapehGrade = Convert.ToDouble(Console.ReadLine());
+            double scienceGrade = Convert.ToDouble(Console.ReadLine());
+            double sibikaGrade = Convert.ToDouble(Console.ReadLine());
+            double espGrade = Convert.ToDouble(Console.ReadLine());
+
+            double average = (englishGrade + mapehGrade + scienceGrade + sibikaGrade + espGrade) / 5;
+            double rounded = Math.Round(average);
+
+            Console.WriteLine("The Average is " + average + " and round off to " + rounded);
+            Console.WriteLine("Press any key to exit......");
+            Console.ReadKey();
+
+        }
+    }
+}
